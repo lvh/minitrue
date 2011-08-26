daba8989cc3b08fb68641983e2d7e4c7a8f1a938
Laurens Van Houtven <_@lvh.cc>
2011-08-26 20:41:35 +0200

    Syntactic decorator sugar

--- minitrue/misdirection.py
+++ minitrue/misdirection.py
@@ -1,6 +1,8 @@
 """
 Mechanism for misdirecting requests to different URLs easily.
 """
+import urlparse
+
 from twisted.python import log
 
 
@@ -23,3 +25,40 @@ def misdirector(f):
         request.uri = misdirected
 
     return requestMangler
+
+
+def replace(url, **kw):
+    split = urlparse.urlsplit(url)._replace(**kw)
+    return urlparse.urlunsplit(split)
+
+
+def affectHostnames(hostnames):
+    def accessor(url):
+        hostname = urlparse.urlsplit(url).hostname
+        if hostname is not None:
+            return hostname.lower()
+
+    return _affect(hostnames, accessor=accessor, affectedMap=str.lower)
+
+
+def affectPaths(paths):
+    def accessor(url):
+        return urlparse.urlsplit(url).path
+
+    return _affect(paths, accessor=accessor)
+
+
+def _affect(affected, accessor=None, affectedMap=None):
+    if affectedMap is not None:
+        affected = map(affectedMap, affected)
+
+    def decorator(md):
+        def decorated(url):
+            this = url if accessor is None else accessor(url)
+
+            if this in affected:
+                return md(url)
+
+        return decorated
+
+    return decorator
